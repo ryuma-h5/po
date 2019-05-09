5f3ecaacdf1e36bac18a379b586f6c7ff29d8a06
Ryuma.h <ryuma810dragon@gmail.com>
2019-05-09 19:23:38 +0900

    Initial Commit

---
+++ Trip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trip
+//
+//  Created by Ryuma Harada on 2019/05/09.
+//  Copyright © 2019 Ryuma Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
